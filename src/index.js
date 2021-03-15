f539f7bd01d1400f1dcf63cd5e54c510844f8ea6
Badimkaa <glopu19@gmail.com>
2021-03-15 22:34:28 +0300

    feat: add solution for the task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+//check('()', [['(', ')']])
+let stack = []
+let brackets = bracketsConfig.flat()
+if (str[0] == brackets[1]) return false
+for (let i = 0; i < str.length; i++) {
+    if (str[i] == brackets[0]) {
+        stack.push(i)
+    }
+    else if (str[i] == brackets[1] ) {
+        stack.pop()
+    }
+}
+if (stack == 0) return true
+else return false
 }
